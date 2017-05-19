6525b741640e3e8f64c99da44b3e8e5a141a546e
anon <gregor.vitek@gmail.com>
2017-05-19 07:51:55 +0200

    Tabs -> Spaces

--- sem.py
+++ sem.py
@@ -4,37 +4,37 @@ import math
 
 #reads points from file
 def read_points(filename):
-	f = open(filename, 'r')
+    f = open(filename, 'r')
 
-	d = f.read()
-	d = d.replace("},{", "\n")
-	d = d.replace("}", "")
-	d = d.replace("{", "")
+    d = f.read()
+    d = d.replace("},{", "\n")
+    d = d.replace("}", "")
+    d = d.replace("{", "")
 
-	points = []
+    points = []
 
-	for line in d.split("\n"):
-		ls = line.split(",")
-		if len(ls) == 3:
-			points.append( tuple(map(float, ls)) )
-	
-	return points
+    for line in d.split("\n"):
+        ls = line.split(",")
+        if len(ls) == 3:
+            points.append( tuple(map(float, ls)) )
+    
+    return points
 
 def generate_points(num_points)	:
-	points = []
-	
-	for i in range(0, num_points):
-		phi = random.uniform(-math.pi, math.pi)
-		theta = random.uniform(0, 2*math.pi)
-		x = math.sin(theta) * math.cos(phi)
-		y = math.sin(theta) * math.sin(phi)
-		z = math.cos(theta)
-		points.append((x,y,z))
-		
-	return points
-		
+    points = []
+    
+    for i in range(0, num_points):
+        phi = random.uniform(-math.pi, math.pi)
+        theta = random.uniform(0, 2*math.pi)
+        x = math.sin(theta) * math.cos(phi)
+        y = math.sin(theta) * math.sin(phi)
+        z = math.cos(theta)
+        points.append((x,y,z))
+        
+    return points
+        
 def main():
-	pass
+    pass
 
 if __name__ == "__main__":
     main()
